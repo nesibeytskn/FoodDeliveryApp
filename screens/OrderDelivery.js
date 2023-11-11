a10ed9f9dc5203a98041fbbd82862fe8575cbebc
nesibeytskn <nesibeyetiskin1@gmail.com>
2023-11-11 14:09:37 +0300

    proje tamamlandı

---
+++ screens/OrderDelivery.js
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const OrderDelivery = () => {
+
+  function renderMap(){
+    return(
+      <View>
+        
+      </View>
+    )
+  }
+  return (
+    <View style={{flex:1, }}>
+      {rederMap()}
+    </View>
+  )
+}
+
+export default OrderDelivery
+
+const styles = StyleSheet.create({})
